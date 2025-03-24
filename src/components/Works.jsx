8bbc63beeb2170c53755ca45097a6e2b6684c34f
MinsandiDeSilva <minsandidesilva21@gmail.com>
2025-03-25 03:22:08 +0530

    tried adding the gosted website

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import Tilt from 'react-parallax-tilt';
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
+import { github  } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -45,6 +45,20 @@ const ProjectCard = ({
               />
             </div>
           </div>
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+
         </div>
 
         <div className='mt-5'>
